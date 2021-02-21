bd19df1975730ed0655587ad8a9abd976a14de73
Sig <sigmonsays@users.noreply.github.com>
2021-02-21 09:06:23 -0800

    add github packaging workflow (#5)
    add github packaging workflow (#5)

---
+++ scripts/build-debian-package.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -eux
+
+BASE_DIR=$(cd $(dirname $(readlink -f $0)) && cd .. && pwd)
+cd ${BASE_DIR}
+
+# Generate changelog
+git_describe="$(git describe --tags)"
+VERSION=${git_describe:1}.$(date +%Y%m%d)+$(lsb_release -cs)
+DATE=$(LC_ALL=C date '+%a, %d %b %Y %H:%M:%S %z')
+
+cat <<EOF > "${BASE_DIR}/debian/changelog"
+git-caddy (${VERSION}) unstable; urgency=medium
+
+  * This is an automated build.
+
+ -- Sigmonsays <noreply@example.net>  ${DATE}
+EOF
+
+apt-get update
+apt-get install -y --no-install-recommends ca-certificates \
+    curl
+
+# Install go
+# https://golang.org/dl/go1.16.linux-amd64.tar.gz
+curl -L https://golang.org/dl/go1.16.linux-amd64.tar.gz | tar -C /usr/local -xzf -
+export PATH=$PATH:/usr/local/go/bin
+
+# Install deps to build.
+mk-build-deps --install --remove \
+  --tool='apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --yes' \
+  "${BASE_DIR}/debian/control"
+
+
+fakeroot debian/rules clean
+fakeroot debian/rules build
+fakeroot debian/rules binary
+
+pwd
+mkdir -p artifact
+cp -v ../*.deb ./artifact/
+ls -l ./artifact/
