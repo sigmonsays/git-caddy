8bace9d9d403b078305f16917a5ca40af25af72f
Sig <sigmonsays@users.noreply.github.com>
2021-02-25 13:36:02 -0800

    auto populate destination and name from git url if not set (#7)
    Co-authored-by: u <u@c1.lxd>

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.15
 require (
 	github.com/fsnotify/fsnotify v1.4.9
 	github.com/sigmonsays/go-logging v0.0.0-20170415192813-93f4813d2694
+	github.com/whilp/git-urls v1.0.0
 	gopkg.in/yaml.v2 v2.4.0
 )
--- repository.go
+++ repository.go
@@ -1,6 +1,12 @@
 package gitcaddy
 
-import "fmt"
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+
+	giturl "github.com/whilp/git-urls"
+)
 
 type Repository struct {
 	Section      string `yaml:"section"`
@@ -20,8 +26,37 @@ func (me *Repository) IsEnabled() bool {
 	}
 	return *me.Enabled
 }
+
 func (me *Repository) Prefix(sub string) string {
 	return fmt.Sprintf("[%s %s] ", me.Name, sub)
+}
+
+func (me *Repository) Defaults() error {
+
+	// fill in name and destination from the remote if possible
+	if me.Name == "" || me.Destination == "" {
+		// // to parse as a url we need a prefix://
+		// i := strings.Index(me.Remote, "//:")
+		// var remote string
+		// if i == -1 {
+		// 	remote = "default://" + me.Remote
+		// } else {
+		// 	remote = me.Remote
+		// }
+		p, err := giturl.Parse(me.Remote)
+		if err != nil {
+			return err
+		}
+		basename := filepath.Base(p.Path)
+		if strings.HasSuffix(basename, ".git") {
+			basename = strings.TrimSuffix(basename, ".git")
+		}
+		me.Name = basename
+		me.Destination = "./" + basename
+
+		return nil
+	}
+	return nil
 }
 
 func (me *Repository) Validate() error {
