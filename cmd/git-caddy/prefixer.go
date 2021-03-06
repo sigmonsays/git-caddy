de7e77763c014260e34dd1dc3541c10166ecaa8a
Sig Lange <sig.lange@gmail.com>
2021-03-06 06:21:21 -0800

    remove unused code

--- cmd/git-caddy/prefixer.go
+++ cmd/git-caddy/prefixer.go
@@ -9,7 +9,6 @@ import (
 type Prefixer struct {
 	writer io.Writer
 	prefix string
-	first  bool
 }
 
 // New creates a new instance of Prefixer.
@@ -21,10 +20,6 @@ func NewPrefixWriter(w io.Writer, prefix string) *Prefixer {
 }
 
 func (me *Prefixer) Write(p []byte) (n int, err error) {
-	// if me.first == false {
-	// 	fmt.Fprintf(me.writer, "\n")
-	// 	me.first = true
-	// }
 	parts := strings.Split(string(p), "\n")
 	for _, part := range parts {
 		fmt.Fprintf(me.writer, "%s %s\n", me.prefix, part)
