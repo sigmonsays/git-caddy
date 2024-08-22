28781be2e513346f59d25fb659d4598da40e8fa4
Sig Lange <sig.lange@gmail.com>
2024-08-22 09:30:19 -0700

    get remote hash, local hash and branch name

--- cmd/git-caddy/pull.go
+++ cmd/git-caddy/pull.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 
 	gc "github.com/sigmonsays/git-caddy"
@@ -17,7 +18,25 @@ type Pull struct {
 func (me *Pull) Run(ctx *gc.Context) error {
 
 	// TODO collect hashes
-	// lhash, err := GetLocalHash(dir string, branch string)
+	branch, err := GetUpstreamBranch(me.Repo.Destination)
+	if err == nil {
+		ctx.UpstreamBranchName = branch
+		log.Tracef("%s upstream branch %s", me.Repo.Name, branch)
+	}
+	lhash, err := GetLocalHash(me.Repo.Destination, branch)
+	if err == nil {
+		ctx.LocalHash = lhash
+		log.Tracef("%s local hash %s", me.Repo.Name, lhash)
+	} else {
+		log.Warnf("%s get local hash: %s", me.Repo.Name, err)
+	}
+	rhash, err := GetRemoteHash(me.Repo.Destination, branch)
+	if err == nil {
+		ctx.RemoteHash = rhash
+		log.Tracef("%s remote hash %s", me.Repo.Name, rhash)
+	} else {
+		log.Warnf("%s get remote hash: %s", me.Repo.Name, err)
+	}
 
 	// perform git pull
 	cmdline := []string{
@@ -26,22 +45,54 @@ func (me *Pull) Run(ctx *gc.Context) error {
 		"-q", "--no-edit", "--all",
 	}
 	log.Tracef("git pull %s", me.Repo.Name)
-
-	log.Tracef("git pull command %v", cmdline)
-	c := exec.Command(cmdline[0], cmdline[1:]...)
-	c.Stdout = NewPrefixWriter(os.Stdout, me.Repo.Prefix("pull"))
-	c.Stderr = NewPrefixWriter(os.Stderr, me.Repo.Prefix("pull"))
-	c.Dir = me.Repo.Destination
-	c.Env = populateEnv(c.Env, me.Cfg, me.Repo)
-	err := c.Run()
+	err = RepoCommand("pull", me.Cfg, me.Repo, cmdline)
 	if err != nil {
 		return NewRepoError("Pull", me.Repo.Name).WithError(err)
 	}
 	return nil
 }
 
+func RepoCommand(prefix string, cfg *gc.Config, repo *gc.Repository, cmdline []string) error {
+	log.Tracef("repo command: %v", cmdline)
+	c := exec.Command(cmdline[0], cmdline[1:]...)
+	c.Stdout = NewPrefixWriter(os.Stdout, repo.Prefix(prefix))
+	c.Stderr = NewPrefixWriter(os.Stderr, repo.Prefix(prefix))
+	c.Dir = repo.Destination
+	c.Env = populateEnv(c.Env, cfg, repo)
+	err := c.Run()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// get remote branch name
+func GetUpstreamBranch(dir string) (string, error) {
+	cmdline := []string{
+		"ls-remote",
+		"-h",
+		".",
+	}
+	c := exec.Command("git", cmdline...)
+	c.Dir = dir
+
+	out, err := c.Output()
+	if err != nil {
+		log.Tracef("get_upstream_branch: [cmdline %s] error: %s\n", cmdline, err)
+		return "", nil
+	}
+	lines := strings.Split(string(out), "\n")
+	if len(lines) == 0 {
+		return "", fmt.Errorf("empty output")
+	}
+	line := lines[0]
+	branch := filepath.Base(line)
+
+	return branch, nil
+}
+
 // get a commit hash for branch in git repo at directory
-func GetLocalHash(dir, branch string) string {
+func GetLocalHash(dir, branch string) (string, error) {
 	cmdline := []string{
 		"-C", dir,
 		"rev-list",
@@ -50,13 +101,13 @@ func GetLocalHash(dir, branch string) string {
 	}
 	out, err := exec.Command("git", cmdline...).Output()
 	if err != nil {
-		log.Infof("remote_hash: [cmdline %s] error: %s\n", cmdline, err)
-		return ""
+		log.Tracef("local_hash: [cmdline %s] error: %s\n", cmdline, err)
+		return "", err
 	}
-	return strings.Trim(string(out), "\n")
+	return strings.Trim(string(out), "\n"), nil
 }
 
-func GetRemoteHash(dir, branch string) string {
+func GetRemoteHash(dir, branch string) (string, error) {
 	cmdline := []string{
 		"-C", dir,
 		"ls-remote",
@@ -66,12 +117,13 @@ func GetRemoteHash(dir, branch string) string {
 	}
 	out, err := exec.Command("git", cmdline...).Output()
 	if err != nil {
-		log.Infof("remote_hash: [cmdline %s] error: %s\n", cmdline, err)
-		return ""
+		log.Tracef("remote_hash: [cmdline %s] error: %s\n", cmdline, err)
+		return "", err
 	}
+
 	tmp := strings.Fields(string(out))
 	if len(tmp) < 1 {
-		return ""
+		return "", fmt.Errorf("Invalid input")
 	}
-	return tmp[0]
+	return tmp[0], nil
 }
--- context.go
+++ context.go
@@ -3,6 +3,10 @@ package gitcaddy
 // state we track per repo, between each step
 type Context struct {
 	RepoExists bool
+
+	// the remote branch name, as if you just cloned the repo
+	UpstreamBranchName string
+
 	LocalHash  string
 	RemoteHash string
 }
