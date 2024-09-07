f884425ddda69c5fc583e3da5844b6ada9d5ba23
Sig Lange <sig.lange@gmail.com>
2024-09-07 12:57:33 +0000

    tweak control socket path

--- cmd/git-caddy/env.go
+++ cmd/git-caddy/env.go
@@ -35,7 +35,7 @@ func makeControlSocket(cfg *gc.Config, r *gc.Repository, ident string) string {
 	b := filepath.Base(ident)
 	ret := " -oControlMaster=auto "
 	ret += " -oControlPersist=yes "
-	ret += " -oControlPath=/tmp/ssh-git-caddy-%u-%h-%n:%p-" + b
+	ret += " -oControlPath=/tmp/ssh-git-caddy-%u-%h-%n-%p-" + b
 	return ret
 }
 func populateEnv(e []string, cfg *gc.Config, r *gc.Repository) []string {
